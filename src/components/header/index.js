7f24b4743a901a5fe38d20e257e5b5a2d86d7824
bikashpradhan <60884514+prabik98@users.noreply.github.com>
2021-12-02 17:24:27 +0530

    apps menu icon

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './header.css';
+import Web from './web/index';
+import Mobile from './mobile/index';
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className="header">
             <div className='logo'>Bikash Pradhan</div>
             <div className='menu'>
-                <div className='web-menu'>Web</div>
-                <div className='mobile-menu'>Mobile</div>
+                <div className='web-menu'>
+                    <Web />
+                </div>
+                <div className='mobile-menu'>
+                    <div onClick={() => setIsOpen(!isOpen)}>
+                        <i class="fi-rr-Apps"></i>
+                    </div>
+                    {isOpen && <Mobile />}
+                </div>
             </div>
         </div>
     );
